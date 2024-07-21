2ac993a33eddfc3b360eb2b0adb65d66c60becd3
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-07-21 18:48:43 +0545

    implemented logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Block from './components/Block';
 
 function App() {
+  const[state,SetState]=useState(Array(9).fill(null));
+  const[currentTurn,SetCurrentTurn]=useState("X")
+
+  const handleBlockClick = (index: number) =>{
+   const stateCopy = Array.from(state)
+   stateCopy[index]=currentTurn;
+   SetCurrentTurn(currentTurn === "X" ? "0" : "X");
+   SetState(stateCopy);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <div className="row">
+        <Block onClick={()=>handleBlockClick(0)} value={state[0]} />
+        <Block onClick={()=>handleBlockClick(1)} value={state[1]} />
+        <Block onClick={()=>handleBlockClick(2)} value={state[2]} />
+      </div>
+      <div className="row">
+        <Block onClick={()=>handleBlockClick(3)} value={state[3]} />
+        <Block onClick={()=>handleBlockClick(4)} value={state[4]} />
+        <Block onClick={()=>handleBlockClick(5)} value={state[5]} />
+      </div>
+      <div className="row">
+        <Block onClick={()=>handleBlockClick(6)} value={state[6]} />
+        <Block onClick={()=>handleBlockClick(7)} value={state[7]} />
+        <Block onClick={()=>handleBlockClick(8)} value={state[8]} />
+      </div>
     </div>
   );
 }
