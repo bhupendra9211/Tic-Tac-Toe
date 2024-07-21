105f5ac9e3283e8df2a47eb67b8f27a6a8665b2c
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-07-21 22:10:01 +0545

    implemented tictactoe logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,41 @@ import React, { useState } from 'react';
 import './App.css';
 import Block from './components/Block';
 
-function App() {
-  const[state,SetState]=useState(Array(9).fill(null));
-  const[currentTurn,SetCurrentTurn]=useState("X")
+function App( ) {
+  const[state,setState]=useState(Array(9).fill(null));
+  const[currentTurn,setCurrentTurn]=useState("X")
+
+  const checkWinner = (state:any[]) =>{
+    const win = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for(let i=0; i<win.length; i++){
+      const[a,b,c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a]==state[c]) return true;
+    }
+    return false;
+  }
 
   const handleBlockClick = (index: number) =>{
    const stateCopy = Array.from(state)
+   if(stateCopy[index] !== null) return;
+
    stateCopy[index]=currentTurn;
-   SetCurrentTurn(currentTurn === "X" ? "0" : "X");
-   SetState(stateCopy);
+
+   const win = checkWinner(stateCopy);
+   if(win){
+    alert(`${currentTurn} won the game`)
+   }
+
+   setCurrentTurn(currentTurn === "X" ? "0" : "X");
+   setState(stateCopy);
   }
   return (
     <div className="board">
